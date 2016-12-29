ac09364b33b5009f594a6d806c0b6832feba084a
tao-xinxiu <isabelle.tao@orange.com>
2016-12-29 14:50:12 +0100

    log accounts service url; throw AccountNotFoundException when searchByName get 404

--- src/main/java/io/pivotal/microservices/services/web/WebAccountsService.java
+++ src/main/java/io/pivotal/microservices/services/web/WebAccountsService.java
@@ -8,6 +8,7 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
@@ -30,13 +31,12 @@ public class WebAccountsService {
 	@Value("${accounts.host:''}")
 	protected String serviceUrl;
 
-	protected Logger logger = Logger.getLogger(WebAccountsService.class
-			.getName());
+	protected Logger logger = Logger.getLogger(WebAccountsService.class.getName());
 
 	@PostConstruct
 	public void init() {
-		this.serviceUrl = serviceUrl.startsWith("http") ? serviceUrl
-				: "http://" + serviceUrl;
+		this.serviceUrl = serviceUrl.startsWith("http") ? serviceUrl : "http://" + serviceUrl;
+		logger.info("Using accounts service url: " + serviceUrl);
 	}
 
 	/**
@@ -48,41 +48,35 @@ public class WebAccountsService {
 	public void demoOnly() {
 		// Can't do this in the constructor because the RestTemplate injection
 		// happens afterwards.
-		logger.warning("The RestTemplate request factory is "
-				+ restTemplate.getRequestFactory().getClass());
+		logger.warning("The RestTemplate request factory is " + restTemplate.getRequestFactory().getClass());
 	}
 
 	public Account findByNumber(String accountNumber) {
-
 		logger.info("findByNumber() invoked: for " + accountNumber);
-		return restTemplate.getForObject(serviceUrl + "/accounts/{number}",
-				Account.class, accountNumber);
+		try {
+			Account account = restTemplate.getForObject(serviceUrl + "/accounts/{number}", Account.class,
+					accountNumber);
+			if (account == null)
+				throw new AccountNotFoundException(accountNumber);
+			return account;
+		} catch (HttpClientErrorException e) {
+			if (e.getStatusCode().equals(HttpStatus.NOT_FOUND)) {
+				throw new AccountNotFoundException(accountNumber);
+			}
+			throw e;
+		}
 	}
 
 	public List<Account> byOwnerContains(String name) {
 		logger.info("byOwnerContains() invoked:  for " + name);
-		Account[] accounts = null;
-
 		try {
-			accounts = restTemplate.getForObject(serviceUrl
-					+ "/accounts/owner/{name}", Account[].class, name);
-		} catch (HttpClientErrorException e) { // 404
-			// Nothing found
-		}
-
-		if (accounts == null || accounts.length == 0)
-			return null;
-		else
+			Account[] accounts = restTemplate.getForObject(serviceUrl + "/accounts/owner/{name}", Account[].class,
+					name);
+			if (accounts == null || accounts.length == 0)
+				return null;
 			return Arrays.asList(accounts);
-	}
-
-	public Account getByNumber(String accountNumber) {
-		Account account = restTemplate.getForObject(serviceUrl
-				+ "/accounts/{number}", Account.class, accountNumber);
-
-		if (account == null)
-			throw new AccountNotFoundException(accountNumber);
-		else
-			return account;
+		} catch (HttpClientErrorException e) {
+			return null;
+		}
 	}
 }
